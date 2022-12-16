079b0904e327a5b7d8b095fe8c92cea094e9f747
Ailierrivero <ailierrivero@gmail.com>
2022-12-16 16:53:59 -0500

    update flavor_amplitudes and test

--- bottomfw/common/flavor_amplitudes.py
+++ bottomfw/common/flavor_amplitudes.py
@@ -23,16 +23,20 @@ class FlavorAmplitudes():
     
     def __init__(self, baryons):
 
-        self.m_baryons = baryons  # baryons podria ser omegas, sigmas, lambdas, etc
-        self.u = Matrix([[1],[0],[0],[0]]) # up quark state
-        self.d = Matrix([[0],[1],[0],[0]]) # down quark state
-        self.s = Matrix([[0],[0],[1],[0]]) # strange quark state
-        self.b = Matrix([[0],[0],[0],[1]]) # bottom quark state
+        self.m_baryons = baryons  
+        # baryons podria ser omegas, sigmas, lambdas, etc
+        self.u = Matrix([[1],[0],[0],[0]]) 
+        # up quark state
+        self.d = Matrix([[0],[1],[0],[0]]) 
+        # down quark state
+        self.s = Matrix([[0],[0],[1],[0]]) 
+        # strange quark state
+        self.b = Matrix([[0],[0],[0],[1]]) 
+        # bottom quark state
        
-
-    
-    
-    def magnetic_operators(self, index): #mu(i)
+ 
+    def magnetic_operators(self, index): 
+        #mu(i)
         """
         Method to calculate the Magnetic Operators
         index == operator index (1,2,3)
@@ -47,38 +51,36 @@ class FlavorAmplitudes():
         return eye(dim)
 
 
-    def tensor_product_operator_1(self, index=1, dim=4): # Mu(1):
+    def tensor_product_operator_1(self, index=1, dim=4): 
+        # Mu(1):
         """
         Method to define the direct product 2⊗2⊗2 magnetic operator for mu_1 
         """
         return TensorProduct(TensorProduct(self.magnetic_operators(index), self.identity_matrix(dim)), self.identity_matrix(dim))
 
-    def tensor_product_operator_2(self, index=2, dim=4): # Mu(2):
+    def tensor_product_operator_2(self, index=2, dim=4): 
+        # Mu(2):
         """
         Method to define the direct product 2⊗2⊗2 magnetic operator for mu_2 
         """
         return TensorProduct(TensorProduct(self.identity_matrix(dim), self.magnetic_operators(index)), self.identity_matrix(dim))
 
-    def tensor_product_operator_3(self, index=3, dim=4): # Mu(3):
+    def tensor_product_operator_3(self, index=3, dim=4): 
+        # Mu(3):
         """
         Method to define the direct product 2⊗2⊗2 magnetic operator for mu_3 
         """
         return TensorProduct(TensorProduct(self.identity_matrix(dim), self.identity_matrix(dim)), self.magnetic_operators(index))        
     
 
-    def flavor_states(self, state_a, state_b, state_c): # Fs
+    def flavor_state(self, state_a, state_b, state_c): 
+        # Fs
         """
         Method to calculate the flavor states
         state_a,b,c should be sympy matrices of the form: Matrix([[0],...,[1]])
         """
         return TensorProduct(TensorProduct(state_a ,state_b), state_c)
 
-    def flavor_state(self, state_a, state_b, state_c): # Fs
-        """
-        Method to calculate the flavor states
-        state_a,b,c should be sympy matrices of the form: Matrix([[0],...,[1]])
-        """
-        return TensorProduct(TensorProduct(state_a ,state_b), state_c)
     
     def Sigma_0(self):
         """
@@ -121,4 +123,4 @@ class FlavorAmplitudes():
         """
         Method to calculate the matrix element for the flavor part
         """
-        return conjugate(transpose(self.Lambda_0))*self.tensor_product_operator_1(index=1, dim=4)*self.Sigma_0
+        return conjugate(transpose(self.Lambda_0))*self.tensor_product_operator_1()*self.Sigma_0
