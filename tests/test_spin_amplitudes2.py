e99c2e1f755d2b8ab580b4c265dc179923b24f31
Andres Ramirez <andres.ramirez.morales@cern.ch>
2023-01-12 12:23:49 +0900

    update

--- tests/test_spin_amplitudes2.py
+++ tests/test_spin_amplitudes2.py
@@ -1,118 +1,35 @@
-# new tests
-
-from bottomfw.common.spin_amplitudes import SpinAmplitudes
-
-test_amplitudes = SpinAmplitudes(baryons="omegas")
-
-test_amp_slo1_s1_2_s3_2 = test_amplitudes.matrix_elements(n=1, x_i=test_amplitudes.symmetric_states(m_proj=1/2), y_j=test_amplitudes.symmetric_states(m_proj=3/2))
-print(test_amp_slo1_s1_2_s3_2)
-
-test_amp_slo1_s1_2m_s1_2 = test_amplitudes.matrix_elements(n=1, x_i=test_amplitudes.symmetric_states(m_proj=-1/2), y_j=test_amplitudes.symmetric_states(m_proj=1/2))
-print(test_amp_slo1_s1_2m_s1_2)
-
-test_amp_slo1_s3_2m_s1_2m = test_amplitudes.matrix_elements(n=1, x_i=test_amplitudes.symmetric_states(m_proj=-3/2), y_j=test_amplitudes.symmetric_states(m_proj=-1/2))
-print(test_amp_slo1_s3_2m_s1_2m)
-
-test_amp_slo1_s1_2m_r1_2 = test_amplitudes.matrix_elements(n=1, x_i=test_amplitudes.symmetric_states(m_proj=-1/2), y_j=test_amplitudes.rho_states(m_proj=1/2))
-print(test_amp_slo1_s1_2m_r1_2)
-
-test_amp_slo1_s3_2m_r1_2m = test_amplitudes.matrix_elements(n=1, x_i=test_amplitudes.symmetric_states(m_proj=-3/2), y_j=test_amplitudes.rho_states(m_proj=-1/2))
-print(test_amp_slo1_s3_2m_r1_2m)
-
-test_amp_slo1_s1_2m_l1_2 = test_amplitudes.matrix_elements(n=1, x_i=test_amplitudes.symmetric_states(m_proj=-1/2), y_j=test_amplitudes.lambda_states(m_proj=1/2))
-print(test_amp_slo1_s1_2m_l1_2)
-
-test_amp_slo1_s3_2m_l1_2m = test_amplitudes.matrix_elements(n=1, x_i=test_amplitudes.symmetric_states(m_proj=-3/2), y_j=test_amplitudes.lambda_states(m_proj=-1/2))
-print(test_amp_slo1_s3_2m_l1_2m)
-
-test_amp_slo1_r1_2_s3_2 = test_amplitudes.matrix_elements(n=1, x_i=test_amplitudes.rho_states(m_proj=1/2), y_j=test_amplitudes.symmetric_states(m_proj=3/2))
-print(test_amp_slo1_r1_2_s3_2)
-
-test_amp_slo1_r1_2m_s1_2 = test_amplitudes.matrix_elements(n=1, x_i=test_amplitudes.rho_states(m_proj=-1/2), y_j=test_amplitudes.symmetric_states(m_proj=1/2))
-print(test_amp_slo1_r1_2m_s1_2)
-
-test_amp_slo1_r1_2m_l1_2 = test_amplitudes.matrix_elements(n=1, x_i=test_amplitudes.rho_states(m_proj=-1/2), y_j=test_amplitudes.lambda_states(m_proj=1/2))
-print(test_amp_slo1_r1_2m_l1_2)
-
-test_amp_slo1_l1_2_s3_2 = test_amplitudes.matrix_elements(n=1, x_i=test_amplitudes.lambda_states(m_proj=1/2), y_j=test_amplitudes.symmetric_states(m_proj=3/2))
-print(test_amp_slo1_l1_2_s3_2)
-
-test_amp_slo1_l1_2m_s1_2 = test_amplitudes.matrix_elements(n=1, x_i=test_amplitudes.lambda_states(m_proj=-1/2), y_j=test_amplitudes.symmetric_states(m_proj=1/2))
-print(test_amp_slo1_l1_2m_s1_2)
-
-test_amp_slo1_l1_2m_r1_2 = test_amplitudes.matrix_elements(n=1, x_i=test_amplitudes.lambda_states(m_proj=-1/2), y_j=test_amplitudes.rho_states(m_proj=1/2))
-print(test_amp_slo1_l1_2m_r1_2)
-
-test_amp_slo1_l1_2m_l1_2 = test_amplitudes.matrix_elements(n=1, x_i=test_amplitudes.lambda_states(m_proj=-1/2), y_j=test_amplitudes.lambda_states(m_proj=1/2))
-print(test_amp_slo1_l1_2m_l1_2)
+import unittest
+import pytest
 
 
-test_amp_slo2_s1_2_s3_2 = test_amplitudes.matrix_elements(n=2, x_i=test_amplitudes.symmetric_states(m_proj=1/2), y_j=test_amplitudes.symmetric_states(m_proj=3/2))
-print(test_amp_slo2_s1_2_s3_2)
+class TestBoostedSVM(unittest.TestCase):
+    """Tests classes in common_methods"""
+    @pytest.fixture(autouse=True)
+    def initdir(self, tmpdir):
+        print(tmpdir)
+        tmpdir.chdir()
 
-test_amp_slo2_s1_2m_s1_2 = test_amplitudes.matrix_elements(n=2, x_i=test_amplitudes.symmetric_states(m_proj=-1/2), y_j=test_amplitudes.symmetric_states(m_proj=1/2))
-print(test_amp_slo2_s1_2m_s1_2)
+    def test_matrix_elements(self):
+        """
+        Test the matrix element method
+        """
+        from bottomfw.common.spin_amplitudes import SpinAmplitudes
+        test_amplitudes = SpinAmplitudes(baryons="omegas")
+        test_amp_slo1_s1_2_s3_2 = test_amplitudes.matrix_elements(n=1, x_i=test_amplitudes.symmetric_states(m_proj=1/2), y_j=test_amplitudes.symmetric_states(m_proj=3/2))
+        test_amp_slo1_s1_2m_s1_2 = test_amplitudes.matrix_elements(n=1, x_i=test_amplitudes.symmetric_states(m_proj=-1/2), y_j=test_amplitudes.symmetric_states(m_proj=1/2))
+        test_amp_slo1_s3_2m_s1_2m = test_amplitudes.matrix_elements(n=1, x_i=test_amplitudes.symmetric_states(m_proj=-3/2), y_j=test_amplitudes.symmetric_states(m_proj=-1/2))
+        test_amp_slo1_s1_2m_r1_2 = test_amplitudes.matrix_elements(n=1, x_i=test_amplitudes.symmetric_states(m_proj=-1/2), y_j=test_amplitudes.rho_states(m_proj=1/2))
+        test_amp_slo1_s3_2m_r1_2m = test_amplitudes.matrix_elements(n=1, x_i=test_amplitudes.symmetric_states(m_proj=-3/2), y_j=test_amplitudes.rho_states(m_proj=-1/2))
 
-test_amp_slo2_s3_2m_s1_2m = test_amplitudes.matrix_elements(n=2, x_i=test_amplitudes.symmetric_states(m_proj=-3/2), y_j=test_amplitudes.symmetric_states(m_proj=-1/2))
-print(test_amp_slo2_s3_2m_s1_2m)
+        self.assertAlmostEqual(test_amp_slo1_s1_2_s3_2, 0.5773502691896257)
+        self.assertAlmostEqual(test_amp_slo1_s1_2m_s1_2, 0.6666666666666666)
+        self.assertAlmostEqual(test_amp_slo1_s3_2m_s1_2m, 0.5773502691896257)
+        self.assertAlmostEqual(test_amp_slo1_s1_2m_r1_2, 0.408248290463863)
+        self.assertAlmostEqual(test_amp_slo1_s3_2m_r1_2m, 0.7071067811865476)
 
-test_amp_slo2_s1_2m_r1_2 = test_amplitudes.matrix_elements(n=2, x_i=test_amplitudes.symmetric_states(m_proj=-1/2), y_j=test_amplitudes.rho_states(m_proj=1/2))
-print(test_amp_slo2_s1_2m_r1_2)
-
-test_amp_slo2_s3_2m_r1_2m = test_amplitudes.matrix_elements(n=2, x_i=test_amplitudes.symmetric_states(m_proj=-3/2), y_j=test_amplitudes.rho_states(m_proj=-1/2))
-print(test_amp_slo2_s3_2m_r1_2m)
-
-test_amp_slo2_s1_2m_l1_2 = test_amplitudes.matrix_elements(n=2, x_i=test_amplitudes.symmetric_states(m_proj=-1/2), y_j=test_amplitudes.lambda_states(m_proj=1/2))
-print(test_amp_slo2_s1_2m_l1_2)
-
-test_amp_slo2_s3_2m_l1_2m = test_amplitudes.matrix_elements(n=2, x_i=test_amplitudes.symmetric_states(m_proj=-3/2), y_j=test_amplitudes.lambda_states(m_proj=-1/2))
-print(test_amp_slo2_s3_2m_l1_2m)
-
-test_amp_slo2_r1_2_s3_2 = test_amplitudes.matrix_elements(n=2, x_i=test_amplitudes.rho_states(m_proj=1/2), y_j=test_amplitudes.symmetric_states(m_proj=3/2))
-print(test_amp_slo2_r1_2_s3_2)
-
-test_amp_slo2_r1_2m_s1_2 = test_amplitudes.matrix_elements(n=2, x_i=test_amplitudes.rho_states(m_proj=-1/2), y_j=test_amplitudes.symmetric_states(m_proj=1/2))
-print(test_amp_slo2_r1_2m_s1_2)
-
-test_amp_slo2_r1_2m_l1_2 = test_amplitudes.matrix_elements(n=2, x_i=test_amplitudes.rho_states(m_proj=-1/2), y_j=test_amplitudes.lambda_states(m_proj=1/2))
-print(test_amp_slo2_r1_2m_l1_2)
-
-test_amp_slo2_l1_2_s3_2 = test_amplitudes.matrix_elements(n=2, x_i=test_amplitudes.lambda_states(m_proj=1/2), y_j=test_amplitudes.symmetric_states(m_proj=3/2))
-print(test_amp_slo2_l1_2_s3_2)
-
-test_amp_slo2_l1_2m_s1_2 = test_amplitudes.matrix_elements(n=2, x_i=test_amplitudes.lambda_states(m_proj=-1/2), y_j=test_amplitudes.symmetric_states(m_proj=1/2))
-print(test_amp_slo2_l1_2m_s1_2)
-
-test_amp_slo2_l1_2m_r1_2 = test_amplitudes.matrix_elements(n=2, x_i=test_amplitudes.lambda_states(m_proj=-1/2), y_j=test_amplitudes.rho_states(m_proj=1/2))
-print(test_amp_slo2_l1_2m_r1_2)
-
-test_amp_slo2_l1_2m_l1_2 = test_amplitudes.matrix_elements(n=2, x_i=test_amplitudes.lambda_states(m_proj=-1/2), y_j=test_amplitudes.lambda_states(m_proj=1/2))
-print(test_amp_slo2_l1_2m_l1_2)
+        
+if __name__ == '__main__':
+    unittest.main()
 
 
-test_amp_slo3_s1_2_s3_2 = test_amplitudes.matrix_elements(n=3, x_i=test_amplitudes.symmetric_states(m_proj=1/2), y_j=test_amplitudes.symmetric_states(m_proj=3/2))
-print(test_amp_slo3_s1_2_s3_2)
-
-test_amp_slo3_s1_2m_s1_2 = test_amplitudes.matrix_elements(n=3, x_i=test_amplitudes.symmetric_states(m_proj=-1/2), y_j=test_amplitudes.symmetric_states(m_proj=1/2))
-print(test_amp_slo3_s1_2m_s1_2)
-
-test_amp_slo3_s3_2m_s1_2m = test_amplitudes.matrix_elements(n=3, x_i=test_amplitudes.symmetric_states(m_proj=-3/2), y_j=test_amplitudes.symmetric_states(m_proj=-1/2))
-print(test_amp_slo3_s3_2m_s1_2m)
-
-test_amp_slo3_s1_2m_l1_2 = test_amplitudes.matrix_elements(n=3, x_i=test_amplitudes.symmetric_states(m_proj=-1/2), y_j=test_amplitudes.lambda_states(m_proj=1/2))
-print(test_amp_slo3_s1_2m_l1_2)
-
-test_amp_slo3_s3_2m_l1_2m = test_amplitudes.matrix_elements(n=3, x_i=test_amplitudes.symmetric_states(m_proj=-3/2), y_j=test_amplitudes.lambda_states(m_proj=-1/2))
-print(test_amp_slo3_s3_2m_l1_2m)
-
-test_amp_slo3_r1_2m_r1_2 = test_amplitudes.matrix_elements(n=3, x_i=test_amplitudes.rho_states(m_proj=-1/2), y_j=test_amplitudes.rho_states(m_proj=1/2))
-print(test_amp_slo3_r1_2m_r1_2)
-
-test_amp_slo3_l1_2_s3_2 = test_amplitudes.matrix_elements(n=3, x_i=test_amplitudes.lambda_states(m_proj=1/2), y_j=test_amplitudes.symmetric_states(m_proj=3/2))
-print(test_amp_slo3_l1_2_s3_2)
-
-test_amp_slo3_l1_2m_s1_2 = test_amplitudes.matrix_elements(n=3, x_i=test_amplitudes.lambda_states(m_proj=-1/2), y_j=test_amplitudes.symmetric_states(m_proj=1/2))
-print(test_amp_slo3_l1_2m_s1_2)
-
-test_amp_slo3_l1_2m_l1_2 = test_amplitudes.matrix_elements(n=3, x_i=test_amplitudes.lambda_states(m_proj=-1/2), y_j=test_amplitudes.lambda_states(m_proj=1/2))
-print(test_amp_slo3_l1_2m_l1_2)
+        
