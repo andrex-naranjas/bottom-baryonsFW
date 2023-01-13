56e4702327b7d29b95ff82da9a89ac4ae83b1705
Andres Ramirez <andres.ramirez.morales@cern.ch>
2023-01-13 09:18:25 +0900

    update

--- tests/test_spin_amplitudes2.py
+++ tests/test_spin_amplitudes2.py
@@ -16,6 +16,7 @@ class TestSpinAmplitudes(unittest.TestCase):
         Test the matrix element method
         """
         from bottomfw.common.spin_amplitudes import SpinAmplitudes
+        import numpy as np
         test_amplitudes = SpinAmplitudes(baryons="omegas")
         test_amp_slo1_s1_2_s3_2 = test_amplitudes.matrix_elements(n=1, x_i=test_amplitudes.symmetric_states(m_proj=1/2), y_j=test_amplitudes.symmetric_states(m_proj=3/2))
         test_amp_slo1_s1_2m_s1_2 = test_amplitudes.matrix_elements(n=1, x_i=test_amplitudes.symmetric_states(m_proj=-1/2), y_j=test_amplitudes.symmetric_states(m_proj=1/2))
@@ -23,7 +24,7 @@ class TestSpinAmplitudes(unittest.TestCase):
         test_amp_slo1_s1_2m_r1_2 = test_amplitudes.matrix_elements(n=1, x_i=test_amplitudes.symmetric_states(m_proj=-1/2), y_j=test_amplitudes.rho_states(m_proj=1/2))
         test_amp_slo1_s3_2m_r1_2m = test_amplitudes.matrix_elements(n=1, x_i=test_amplitudes.symmetric_states(m_proj=-3/2), y_j=test_amplitudes.rho_states(m_proj=-1/2))
 
-        self.assertAlmostEqual(test_amp_slo1_s1_2_s3_2, 0.5773502691896257)
+        self.assertAlmostEqual(test_amp_slo1_s1_2_s3_2, np.sqrt(3) / 3)
         self.assertAlmostEqual(test_amp_slo1_s1_2m_s1_2, 0.6666666666666666)
         self.assertAlmostEqual(test_amp_slo1_s3_2m_s1_2m, 0.5773502691896257)
         self.assertAlmostEqual(test_amp_slo1_s1_2m_r1_2, 0.408248290463863)
