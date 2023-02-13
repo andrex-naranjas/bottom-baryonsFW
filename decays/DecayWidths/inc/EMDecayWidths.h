8d465fc9c77f17159fb9345ae326fb3e1998329c
Ailierrivero <ailierrivero@gmail.com>
2023-02-13 07:38:21 -0600

    Added SPINFLIP_U3_GS_GS

--- decays/DecayWidths/inc/EMDecayWidths.h
+++ decays/DecayWidths/inc/EMDecayWidths.h
@@ -54,7 +54,9 @@ private:
   virtual double FI2(double EB, double EWCC, double MA, double k_value);
 
   // SPIN-FLIP Integrals
-  virtual double SPINFLIP_U1_GS_GS(double k_value, double alpha_lam,  double MB, double ML);
+  virtual double SPINFLIP_U1_GS_GS(double k_value, double alpha_lam, double alpha_rho, double MB, double ML);
+  virtual double SPINFLIP_U2_GS_GS(double k_value, double alpha_lam, double alpha_rho, double MB, double ML);
+  virtual double SPINFLIP_U3_GS_GS(double k_value, double alpha_lam, double alpha_rho, double MB, double ML);
 
 };
 
