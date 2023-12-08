b2a660c48299d7eb50c4b7bc7be68b0b456af67c
Ailierrivero <ailierrivero@gmail.com>
2023-12-08 11:41:24 +0100

    Added up to T1_r1_m1_l1_m1_r1_m1

--- decays/DecayWidths/src/EMDecayWidths.cxx
+++ decays/DecayWidths/src/EMDecayWidths.cxx
@@ -1685,6 +1685,37 @@ double EMDecayWidths::SPINFLIP_U3_r1_m1m_l1_m0_r1_m1m(double k_value, double alp
   return value;
 }
 
+// ORBIT-FLIP Tensor operators
+//T1r1m0l1m0r1m1m
+double EMDecayWidths::T1_r1_m0_l1_m0_r1_m1m(double k_value, double alpha_lam, double alpha_rho,
+			  double mbottom, double mlight, double thetak, double phik){
+  double value1 = (-1.0) * std::pow(k_value, 2) / 8.;
+  double value2 = (3. * std::pow(mbottom, 2)) / (std::pow(alpha_lam * (mbottom + 2. * mlight), 2));
+  double value3 = 1./std::pow(alpha_rho, 2);
+  double value = ( (-1.0) * p_imag * std::sqrt(1.5) * std::pow(k_value, 2) * mbottom * std::exp(value1 * (value2 + value3)))/(2. * alpha_lam * (mbottom + 2. * mlight));
+  return value;
+}
+
+//T1r1m1l1m0r1m0
+double EMDecayWidths::T1_r1_m1_l1_m0_r1_m0(double k_value, double alpha_lam, double alpha_rho,
+			  double mbottom, double mlight, double thetak, double phik){
+  double value1 = (-1.0) * std::pow(k_value, 2) / 8.;
+  double value2 = (3. * std::pow(mbottom, 2)) / (std::pow(alpha_lam * (mbottom + 2. * mlight), 2));
+  double value3 = 1./std::pow(alpha_rho, 2);
+  double value = ( (-1.0) * p_imag * std::sqrt(1.5) * std::pow(k_value, 2) * mbottom * std::exp(value1 * (value2 + value3)))/(2. * alpha_lam * (mbottom + 2. * mlight));
+  return value;
+}
+
+//T1r1m1l1m1r1m1
+double EMDecayWidths::T1_r1_m1_l1_m1_r1_m1(double k_value, double alpha_lam, double alpha_rho,
+			  double mbottom, double mlight, double thetak, double phik){
+  double value1 = (-1.0) * std::pow(k_value, 2) / 8.;
+  double value2 = (3. * std::pow(mbottom, 2)) / (std::pow(alpha_lam * (mbottom + 2. * mlight), 2));
+  double value3 = 1./std::pow(alpha_rho, 2);
+  double value = ( p_imag * std::sqrt(1.5) * alpha_lam * std::exp(value1 * (value2 + value3)));
+  return value;
+}
+
 
 
 
