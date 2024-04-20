342ec5f54744a7e9e40fe8aca9dfa704db62e754
Andres Ramirez <stormchildremix@gmail.com>
2024-04-20 12:35:34 -0600

    update

--- decays/decay_utils.py
+++ decays/decay_utils.py
@@ -477,7 +477,7 @@ def print_row_latex(mass_a, masses_b, masses_c, state_name, state_decays, errors
     print(state_name, end='',file=f_out)
     for i in range(nstate):
         value=0
-        if(state_decays[i]==0.0):
+        if(state_decays[i]==0.000000000000000000000):
             if mass_a < masses_b[i] + masses_c[i]: # check if the width is zero for energy conservation
                 cons_energy_count+=1
                 value = '$0$'
